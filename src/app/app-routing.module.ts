1bbd01fbf434d790b8237762757a92eb962fb758
Stepan Tikhonov <stepan.tikhonov@sme.additiv.com>
2023-03-14 13:49:23 +0100

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponent } from 'src/FirstComponent/first.component';
+import { FourthComponent } from 'src/FourthComponent/fourth.component';
+import { SecondComponent } from 'src/SecondComponent/second.component';
+import { ThirdComponent } from 'src/ThirdComponent/third.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'first', component: FirstComponent },
+  { path: 'second', component: SecondComponent },
+  { path: 'third', component: ThirdComponent },
+  { path: 'fourth', component: FourthComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponent } from 'src/FirstComponent/first.component';
+import { SecondComponent } from 'src/SecondComponent/second.component';
+import { ThirdComponent } from 'src/ThirdComponent/third.component';
+import { FourthComponent } from 'src/FourthComponent/fourth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FourthComponent,
+    FirstComponent,
+    SecondComponent,
+    ThirdComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [
+    FirstComponent, SecondComponent, ThirdComponent, FourthComponent
+  ]
 })
 export class AppModule { }
